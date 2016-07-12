86014e59c4aebbc2d0988161ee0e8b26aa6be2c3
marvrez <marvinreza@outlook.com>
2016-07-12 21:14:39 +0200

    The script is now finally possible to use! Using urllib urllib and re to parse the playlist-url

--- PLparser.py
+++ PLparser.py
@@ -1,11 +1,42 @@
 #parse Youtube URL's given the playlist link.
 #usage: python PLparser.py playlist-url
-
-import urllib.error, urllib.request
+#TODO: try implementing threading!
 import re
+import urllib.error
+import urllib.request
 import sys
 import time 
 
+def parse(url):
+    final_url = []
+    all_urls = []
+    if ("list=" in url):
+        eq = url.rfind("=")+1
+        pl_code = url[eq:]
+    else:
+        print("Incorrect playlist, please try again with a correct Youtube playlist URL!")
+        exit(1)
+    try:
+        yt_page = str(urllib.request.urlopen(url).read())
+    except urllib.error.URLError as e:
+        print (e.reason)
+
+    pattern = re.compile(r"watch\?v=\S+?list="+ pl_code)
+    match = re.findall(pattern,yt_page)
+
+    if match:
+        for __video in match:
+            video = str(__video)
+            if "&" in video:
+                amp = video.index("&") 
+                video_url = video[:amp]
+                final_url.append("https://youtube.com/"+ video_url[:amp])
+        all_urls = list(set(final_url))
+        for video in all_urls:
+            print(video)
+    else:
+        print ("No videos found in playlist")
+        exit(1)
 
 
 if __name__ == '__main__':
@@ -16,5 +47,4 @@ if __name__ == '__main__':
         url = sys.argv[1]
         if "https://" not in url:
             url = "https://" + url
-
-        parse(url)
+        parse(url) #leggo
